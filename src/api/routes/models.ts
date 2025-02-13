b2cc1323ca572e2aca7f26ff49a0f90fcac3f459
Vinlic科技 <vinlic@vinlic.com>
2025-02-13 11:01:50 +0800

    补充/models请求中缺失的模型

--- src/api/routes/models.ts
+++ src/api/routes/models.ts
@@ -17,6 +17,51 @@ export default {
                         "id": "deepseek-coder",
                         "object": "model",
                         "owned_by": "deepseek-free-api"
+                    },
+                    {
+                        "id": "deepseek-think",
+                        "object": "model",
+                        "owned_by": "deepseek-free-api"
+                    },
+                    {
+                        "id": "deepseek-r1",
+                        "object": "model",
+                        "owned_by": "deepseek-free-api"
+                    },
+                    {
+                        "id": "deepseek-search",
+                        "object": "model",
+                        "owned_by": "deepseek-free-api"
+                    },
+                    {
+                        "id": "deepseek-r1-search",
+                        "object": "model",
+                        "owned_by": "deepseek-free-api"
+                    },
+                    {
+                        "id": "deepseek-think-search",
+                        "object": "model",
+                        "owned_by": "deepseek-free-api"
+                    },
+                    {
+                        "id": "deepseek-r1-silent",
+                        "object": "model",
+                        "owned_by": "deepseek-free-api"
+                    },
+                    {
+                        "id": "deepseek-search-silent",
+                        "object": "model",
+                        "owned_by": "deepseek-free-api"
+                    },
+                    {
+                        "id": "deepseek-think-fold",
+                        "object": "model",
+                        "owned_by": "deepseek-free-api"
+                    },
+                    {
+                        "id": "deepseek-r1-fold",
+                        "object": "model",
+                        "owned_by": "deepseek-free-api"
                     }
                 ]
             };
